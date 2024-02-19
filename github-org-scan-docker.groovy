689eb053ef3c6332754f133786865ee797f3906f
Sriram Subramanian <sriram@endor.ai>
2024-02-19 13:58:23 +0530

    Adding full package name for StackTraceUtils

--- github-org-scan-docker.groovy
+++ github-org-scan-docker.groovy
@@ -40,7 +40,7 @@ def isCommitNewerThanNDays(projectUrl, numberOfDays) {
           }
     } catch (Exception e) {
       echo "Failed to get Commit Information from the URL."
-      org.codehaus.groovy.runtime.StackTraceUtils.printSanitizedStackTrace(e)
+      org.codehaus.groovy.runtime.StackTraceUtils.sanitize(e).printStackTrace()
       // Marking this as 'true' to mimic the current behavior as well as the behavior when commit time check flag is unchecked
       commitInLastNDays = true
     }
