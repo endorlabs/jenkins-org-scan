c3ab9612acdff478e8f991f72b19f285ba9886dc
Sriram Subramanian <5715204+reachsrirams@users.noreply.github.com>
2024-02-29 21:33:23 +0530

    Change the code flow to allow setting of GH Access token (#13)
    * Adding explicit github token
    
    * setting API version in header
    
    * getting token via credential store
    
    * removing token to test 404
    
    * Unsetting GH Token Env variable
    
    * Setting back GH Token Env variable but not using it
    
    * Setting back GH Token Env variable and using it
    
    * Getting GH token using getenv method and using token instead of Bearer
    
    * Getting GH token using getenv method and using token instead of Bearer
    
    * Getting GH token using env object

--- github-org-scan-docker.groovy
+++ github-org-scan-docker.groovy
@@ -29,7 +29,22 @@ def isCommitNewerThanNDays(projectUrl, numberOfDays) {
     def apiUrl = new URL("https://api.github.com/repos/$repo/commits?per_page=1")
     echo "Fetching commit information using URL - ${apiUrl}"
     try {
-          def response = apiUrl.getText()
+          // Open a connection
+          def connection = apiUrl.openConnection()
+          // Set the request method
+          connection.setRequestMethod('GET')
+          // Get the access token from the environment variable
+          def githubToken = env.GITHUB_TOKEN
+          // Set the access token in the Authorization header
+          connection.setRequestProperty('Authorization', "token ${githubToken}")
+          connection.setRequestProperty('X-GitHub-Api-Version','2022-11-28')
+          // Send the request
+          connection.connect()
+          // Read the response
+          def responseCode = connection.getResponseCode()
+          println "Response code: for $apiUrl is $responseCode"
+
+          def response = connection.inputStream.text
           def json = new JsonSlurper().parseText(response)
           def commitDate = json[0].commit.author.date
           
